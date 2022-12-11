0a275ed8eeef43d0e089a42c3d655dd10330e9e4
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-12-11 21:32:05 +0800

    Adding again the earlier code but in comments

--- Queue data.py
+++ Queue data.py
@@ -14,6 +14,17 @@ class Queue:
     def dequeue(self):
         return self._elements.popleft()
 
+#fifo = Queue()
+#fifo.enqueue("1st")
+#fifo.enqueue("2nd")
+#fifo.enqueue("3rd")
+
+#fifo.dequeue()
+
+#fifo.dequeue()
+
+#fifo.dequeue()
+
 fifo = Queue("1st", "2nd", "3rd")
 print(len(fifo))
 
--- Stack data.py
+++ Stack data.py
@@ -17,3 +17,4 @@ class Stack(Queue):
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
     print(element)
+
