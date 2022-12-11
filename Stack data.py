e06d4631d4d820823338c8969ebe806e348df7e1
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-12-11 21:34:11 +0800

    Created stacks using list and printing out before and after pop

--- Stack data.py
+++ Stack data.py
@@ -14,7 +14,19 @@ class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
 
-lifo = Stack("1st", "2nd", "3rd")
-for element in lifo:
-    print(element)
+#lifo = Stack("1st", "2nd", "3rd")
+#for element in lifo:
+    #print(element)
 
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+print(lifo)
+print(lifo.pop())
+
+print(lifo.pop())
+
+print(lifo.pop())
+print(lifo)
