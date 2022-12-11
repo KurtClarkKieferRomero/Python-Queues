59edf4d4f19138e2339d2258d3923b07c24f4dcb
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-12-11 21:20:48 +0800

    For loop to iteratethe Queue

--- Queue data.py
+++ Queue data.py
@@ -13,3 +13,11 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+
+fifo = Queue("1st", "2nd", "3rd")
+len(fifo)
+
+for element in fifo:
+    print(element)
+
+len(fifo)
