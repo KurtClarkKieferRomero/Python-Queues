ce7cdf2653a4ff3b530ba63359e209ec6f161629
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-12-11 23:48:47 +0800

    Using python's tuple comparison

--- Priority Queue.py
+++ Priority Queue.py
@@ -8,8 +8,13 @@ heappush(fruits, "banana")
 print(fruits)
 
 from heapq import heappop
-
 heappop(fruits)
-
-
 print(fruits)
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+print(person1 < person2)
+
+print(person2 < person3)
