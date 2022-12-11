b66f49d1d1d3a2783cd38aa974ac54d615f82644
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-12-11 21:19:43 +0800

    Iterable with len()

--- Queue data.py
+++ Queue data.py
@@ -1,23 +1,15 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    def __init__(self, *elements):
+        self._elements = deque(elements)
 
-    def enqueue(self, element):
-        self._elements.append(element)
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
 
     def dequeue(self):
         return self._elements.popleft()
-
-
-fifo = Queue()
-fifo.enqueue("1st")
-fifo.enqueue("2nd")
-fifo.enqueue("3rd")
-
-fifo.dequeue()
-
-fifo.dequeue()
-
-fifo.dequeue()
